d8b70070befa26f7026ad30aee56940ab6fdba33
VICTOR FERNANDO CESARONI <victor.fc@puccampinas.edu.br>
2018-08-22 09:28:23 -0300

    IO

--- CompiladoresVM/Form1.cs
+++ CompiladoresVM/Form1.cs
@@ -13,7 +13,7 @@ namespace CompiladoresVM
 {
     public partial class Form1 : Form
     {
-        VMCore vm = new VMCore(200, 50);
+        VMCore vm;
 
         public Form1()
         {
@@ -24,12 +24,14 @@ namespace CompiladoresVM
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
+                vm = new VMCore(2000, 200);
+
                 vm.ParseFromFile(openFileDialog1.FileName);
 
                 listViewMemoryProgram.Items.Clear();
-                for (int i = 0; i < vm.memory.maxInstructions; i++)
+                for (int i = 0; i < vm.instructionCount; i++)
                 {
-                    var ins = vm.memory.Instructions[i];
+                    var ins = vm.instructions[i];
                     listViewMemoryProgram.Items.Add(new ListViewItem(new string[] { i.ToString(), ins.label, ins.opcode, ins.arg1, ins.arg2, ins.comment, ins.useLabel.ToString(), }));
 
                     if (ins.useLabel)
@@ -46,6 +48,9 @@ namespace CompiladoresVM
                         listViewMemoryProgram.Items[i].ForeColor = Color.DarkGray;
                     }
                 }
+                listViewMemoryProgram.Items.Add(new ListViewItem(new string[] { "FIM", "FIM", "FIM", "FIM", "FIM", "FIM", "FIM", }));
+
+                ProccessIO();
             }
         }
 
@@ -74,25 +79,63 @@ namespace CompiladoresVM
         private void UpdateInterface()
         {
             listViewMemoryData.Items.Clear();
-            for (int i = 0; i < vm.memory.M.Count(); i++)
+            for (int i = 0; i < vm.M.Count(); i++)
             {
-                var m = vm.memory.M[i];
+                var m = vm.M[i];
                 listViewMemoryData.Items.Add(new ListViewItem(new string[] { i.ToString(), m.ToString(), }));
             }
+            listViewMemoryData.Items.Add(new ListViewItem(new string[] { "FIM", "FIM", }));
 
-            labelI.Text = "I: " + vm.registers.I.ToString();
-            labelS.Text = "S: " + vm.registers.S.ToString();
+            labelI.Text = "I: " + vm.I.ToString();
+            labelS.Text = "S: " + vm.S.ToString();
 
             listViewMemoryProgram.SelectedItems.Clear();
-            listViewMemoryProgram.Items[vm.registers.I].Selected = true;
-            //listViewMemoryProgram.Items[vm.registers.I].BackColor = Color.Green;
-            listViewMemoryProgram.EnsureVisible(vm.registers.I);
+            listViewMemoryProgram.Items[vm.I].Selected = true;
+            //listViewMemoryProgram.Items[vm.I].BackColor = Color.Green;
+            listViewMemoryProgram.EnsureVisible(vm.I);
             listViewMemoryProgram.Select();
+
+            if (vm.S >= 0)
+            {
+                //listViewMemoryData.SelectedItems.Clear();
+                //listViewMemoryData.Items[vm.S].Selected = true;
+                listViewMemoryData.Items[vm.S].BackColor = Color.Green;
+                //listViewMemoryData.EnsureVisible(vm.S);
+                //listViewMemoryData.Select();
+            }
+        }
+
+        private void ProccessIO()
+        {
+            txtInput.Enabled = vm.io.waitingInput;
+            txtOutput.Enabled = vm.io.waitingOutput;
+
+            int parseResult = 0;
+
+            if (vm.io.waitingOutput)
+            {
+                int ioReadData = vm.IORead();
+
+                txtOutput.Text += ioReadData.ToString() + "\r\n";
+            }
+
+            if (vm.io.waitingInput && int.TryParse(txtInput.Text, out parseResult))
+            {
+                int ioWriteData = int.Parse(txtInput.Text);
+                vm.IOWrite(ioWriteData);
+
+                txtInput.Text = "";
+            }
+
+            txtInput.Enabled = vm.io.waitingInput;
+            txtOutput.Enabled = vm.io.waitingOutput;
         }
 
         private void continuarToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            ProccessIO();
             vm.Run(true, true);
+            ProccessIO();
             UpdateInterface();
         }
 
@@ -104,7 +147,9 @@ namespace CompiladoresVM
             {
                 //txtOutput.Text += vm.memory.M[vm.registers.I].ToString() + "\r\n";                
 
+                ProccessIO();
                 bool continueExec = vm.SingleStep(false);
+                ProccessIO();
 
                 if (!continueExec)
                     break;
